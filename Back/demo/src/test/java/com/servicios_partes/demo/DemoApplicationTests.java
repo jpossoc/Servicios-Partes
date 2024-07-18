26e36a7f09fa0666b22c050dbf5acf344223f2f5
jspossoc <poshose13gmail.com>
2024-07-18 17:09:09 -0500

    back structure

---
+++ Back/demo/src/test/java/com/servicios_partes/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicios_partes.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
